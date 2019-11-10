b6e194bf985a9009760a48ed7d58ea1e494c7186
Marko Grdinić <mrakgr@gmail.com>
2019-11-10 11:39:55 +0100

    "11:30am.
    ```
    let rec R_imm_sum (o' : Policy) (o : Policy) (tree : GameTree) =
        match tree with
        | Terminal _ -> 0.0 // This check is to avoid double counting the Terminal nodes due to the recursion. It is not an issue in the eq 9 test.
        | Response (_, branches) ->
            // As it is assumed that the opponent only has one action in every node, the CFR reach probabilities are all 1 and play no role.
            R_imm o' o tree + Array.fold (fun s branch -> s + R_imm_sum o' o branch) 0.0 branches
    ```
    
    Refactored this so it looks more like the Python version.
    
    Now let me run it.
    
    11:35am. 100k tests pass, no problem. Theorem 3 is sound.
    
    11:40am. Now I also need to put lemma 6 through the same paces. I wonder if it will suddenly start working with the new generator?"

--- CFR In Fsharp/Theorem 3/Core.fsx
+++ CFR In Fsharp/Theorem 3/Core.fsx
@@ -1,4 +1,6 @@
-﻿// Types
+﻿open System.Collections.Generic
+
+// Types
 
 type Infoset = int
 type Size = int
@@ -16,7 +18,7 @@ type Policy = Map<Infoset, PolicyAtNode>
 #r @"..\..\packages\FsCheck.3.0.0-alpha4\lib\net452\FsCheck.dll"
 open FsCheck
 
-type TreePolicies = {policies : Policy []; tree : GameTree; infoset_sizes: Size []}
+type TreePolicies = {policies : Policy []; tree : GameTree }
 
 let gen_policy_at_node s : Gen<PolicyAtNode> = 
     let total = 100000
@@ -30,38 +32,61 @@ let gen_policy infoset_sizes : Gen<Policy> =
 let gen_reward = Gen.choose (-100,100) |> Gen.map (fun x -> float x / 10.0)
 let gen_terminal = gen_reward |> Gen.map Terminal
 
-let gen_tree num_infosets s : Gen<GameTree * Size []> = gen {
-    let! (infoset_sizes : Size []) = Gen.choose (2,5) |> Gen.arrayOfLength num_infosets
-    let rec loop s =
-        Gen.oneof [
-            gen_terminal
-            if s > 0 then
-                gen {
-                    let! (id : Infoset) = Gen.choose (0, infoset_sizes.Length-1)
+/// Picks the ith index not in l.
+let count_and_pick (l : Set<int>) =
+    assert (Set.forall (fun x -> x >= 0) l)
+    let inline gap_empty size next i = if i < size then i else size + next (i - size)
+    let inline gap_one next i = 1 + next i
+    let c, next = Set.foldBack (fun x' (c, next) -> c+1, fun x i -> gap_empty (x' - x - 1) (gap_one (next x')) i) l (0, (fun x' i -> i))
+    c, next -1
+
+let gen_tree : Gen<GameTree * Size []> = Gen.sized <| fun s -> gen {
+    let infoset_sizes = ResizeArray()
+    let rec response infoset_removed s =
+        if s > 0 then
+            gen {
+                let infoset_removed_count, pick_ith = count_and_pick infoset_removed
+                let make_new = gen {
+                    let id = infoset_sizes.Count
+                    let! infoset_size = Gen.choose(2,5)
+                    infoset_sizes.Add infoset_size
+                    return infoset_size, id
+                    }
+
+                let get_existing = gen {
+                    let! id = Gen.choose(0, infoset_sizes.Count-infoset_removed_count-1) |> Gen.map pick_ith
                     let infoset_size = infoset_sizes.[id]
-                    let! branches = Array.init infoset_size (fun _ -> loop (s/infoset_size)) |> Gen.sequenceToArr
-                    return Response(id,branches)
-                }
-            else gen_terminal
-            ]
+                    return infoset_size, id
+                    }
 
-    let! tree = loop s
-    return tree, infoset_sizes
+                let! infoset_size, id = Gen.frequency [(infoset_sizes.Count-infoset_removed_count, get_existing); (max 1 infoset_removed_count, make_new)] 
+
+                let! branches = loop (Set.add id infoset_removed) (s/infoset_size) |> Gen.arrayOfLength infoset_size 
+                return Response(id,branches)
+            }
+        else gen_terminal
+    and loop infoset_removed s = Gen.frequency [(2, response infoset_removed s); (1, gen_terminal)]
+
+    let! tree = response Set.empty s
+    return tree, infoset_sizes.ToArray()
     }
 
-let gen_game num_policies num_infosets size : Gen<TreePolicies> = gen {
-    let! tree, infoset_sizes = gen_tree num_infosets size
+let gen_game num_policies : Gen<TreePolicies> = gen {
+    let! tree, infoset_sizes = gen_tree
     let! policies = gen_policy infoset_sizes |> Gen.arrayOfLength num_policies
-    return {policies=policies; tree=tree; infoset_sizes=infoset_sizes}
+    return {policies=policies; tree=tree}
     }
-
-//gen_game 2 5 10
+    
+//gen_game 2
 //|> Gen.sample 1
 
 type MyGenerators =
-    static member Game = Arb.fromGen (gen_game 2 5 5)
+    static member Game = Arb.fromGen (gen_game 2)
 
 Arb.register<MyGenerators>()
+
+//Arb.generate<TreePolicies>
+//|> Gen.sampleWithSize 6 10
 
 let error_bound_for_floats = 10.0 ** -7.0
 let (=?) a b = abs (a - b) <= error_bound_for_floats
--- CFR In Fsharp/Theorem 3/example.fsx
+++ CFR In Fsharp/Theorem 3/example.fsx
@@ -20,10 +20,9 @@ let R_imm (o' : Policy) (o : Policy) (tree : GameTree) =
 let rec R_imm_sum (o' : Policy) (o : Policy) (tree : GameTree) =
     match tree with
     | Terminal _ -> 0.0 // This check is to avoid double counting the Terminal nodes due to the recursion. It is not an issue in the eq 9 test.
-    | Response _ -> 
-        // No multiplication by the player probabilities. It does not matter whether o' or o is passed to u'.
+    | Response (_, branches) -> 
         // As it is assumed that the opponent only has one action in every node, the CFR reach probabilities are all 1 and play no role.
-        R_imm o' o tree + u' o' (fun _ branch -> R_imm_sum o' o branch) tree
+        R_imm o' o tree + Array.fold (fun s branch -> s + R_imm_sum o' o branch) 0.0 branches
 
 open FsCheck
 
@@ -31,47 +30,7 @@ let ``R_full<=R_imm_sum'`` ({tree=tree; policies=policies} : TreePolicies) =
     let o, o' = policies.[0], policies.[1]
     let left = R_full o' o tree
     let right = R_imm_sum o' o tree
-    left <= right |@ sprintf "Failure. %f <> %f. Error bound for floats is %f." left right error_bound_for_floats
+    left <= right |@ sprintf "%f <= %f" left right
 
 // Fails
-//Check.One({Config.Quick with MaxTest=100000}, ``R_full<=R_imm_sum'``)
-
-// Surprisingly it seems that theorem 3 is false as well.
-// Here is a counter example.
-
-//Falsifiable, after 6543 tests (0 shrinks) (17947101256372067428,14787371683992192641)
-//Last step was invoked with size of 7 and seed of (543175987540267980,12028313537534174055):
-//Label of failing property: Failure. 8.300594 <> 1.965112. Error bound for floats is 0.000000.
-//Original:
-//{ policies =
-//            [|map
-//                [(0, [|0.005; 0.99091; 0.00409|]); (1, [|0.91659; 0.08341|]);
-//                 (2, [|0.17351; 0.23036; 0.33814; 0.25799|]);
-//                 (3, [|0.81159; 0.18841|]);
-//                 (4, [|0.05401; 0.0144; 0.88171; 0.04988|])];
-//              map
-//                [(0, [|0.30333; 0.54771; 0.14896|]); (1, [|0.1455; 0.8545|]);
-//                 (2, [|0.03393; 0.39449; 0.52965; 0.04193|]);
-//                 (3, [|0.15721; 0.84279|]);
-//                 (4, [|0.02714; 0.30085; 0.09428; 0.57773|])]|]
-//  tree =
-//        Response
-//          (1,
-//           [|Terminal -6.3;
-//             Response
-//               (3,[|Response (1,[|Terminal 8.5; Terminal -7.1|]); Terminal 6.5|])|])
-//  infoset_sizes = [|3; 2; 4; 2; 4|] }
-
-let f ({tree=tree; policies=policies} : TreePolicies) =
-    let o, o' = policies.[0], policies.[1]
-    let left = R_full o' o tree
-    let right = R_imm_sum o' o tree
-    left, right
-
-let example =
-    { policies = [|Map [(0, [|1.0; 0.0|]); (1, [|1.0; 0.0|])]
-                   Map [(0, [|0.0; 1.0|]); (1, [|0.0; 1.0|])] |]
-      tree = Response(0, [|Terminal -10.0; Response (1,[|Response (0,[|Terminal 10.0; Terminal -10.0|]); Terminal 10.0|])|])
-      infoset_sizes = [|2; 2|] }
-
-f example
+Check.One({Config.Quick with MaxTest=100000}, ``R_full<=R_imm_sum'``)
