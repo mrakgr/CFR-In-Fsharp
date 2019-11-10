7c490145bc896b5a2198342680672927c006c480
Marko Grdinić <mrakgr@gmail.com>
2019-11-10 14:32:57 +0100

    "2:15pm.
    ```
    let R' (o' : Policy) (o : Policy) (tree : GameTree) =
        match tree with
        | Terminal _ -> 0.0
        | Response (id, branches) ->
            action_max o id (fun i o' ->
                u o' tree - u o tree +
                // According to definition of succ which only considers opponent reach probabilities,
                // I skip multipling by current player reach probabilities.
                // Since perfect recall assures no cycles in infosets `u o' branch = u (action_at i o' id) branch` holds.
                u o' branches.[i] - u o branches.[i]
                )
    ```
    
    It should be done like this...
    
    Now wait, no.
    
    ```
        | Response (id, branches) ->
            action_max o id (fun i o_a ->
                u o_a tree - u o tree +
                // According to definition of succ which only considers opponent reach probabilities,
                // I skip multipling by current player reach probabilities.
                // Since perfect recall assures no cycles in infosets `u o' branch = u (action_at i o' id) branch` holds.
                u o' branches.[i] - u o branches.[i]
                )
    ```
    
    Like this.
    
    2:15pm.
    
    ```
    Label of failing property: 0.000000 <> 0.000000
    Original:
    { policies =
                [|map [(0, [|0.26795; 0.73205|])]; map [(0, [|0.23201; 0.76799|])]|]
      tree = Response (0,[|Terminal -1.2; Terminal -1.2|]) }
    with exception:
    ```
    
    Not a real failure, but numerical inaccuracy is the culprit here.
    
    2:20pm.
    
    ```
    let action_max (o : Policy) (id : Infoset) f =
        let len = o.[id].Length
        let rec loop s i =
            if i < len then
                let a = Array.zeroCreate len
                a.[i] <- 1.0
                loop (max s (f i (Map.add id a o))) (i+1)
            else s
        loop -infinity 0
    
    let R (o' : Policy) (o : Policy) (tree : GameTree) = u o' tree - u o tree
    let R' (o' : Policy) (o : Policy) (tree : GameTree) =
        match tree with
        | Terminal _ -> 0.0
        | Response (id, branches) ->
            action_max o id (fun i o_a ->
                u o_a tree - u o tree +
                // According to definition of succ which only considers opponent reach probabilities,
                // I skip multipling by current player reach probabilities.
                u o' branches.[i] - u o branches.[i]
                )
    
    open FsCheck
    
    let ``R=R'`` ({tree=tree; policies=policies} : TreePolicies) =
        let o, o' = policies.[0], policies.[1]
        let left = R o' o tree
        let right = R' o' o tree
        // Tests for equality and prints an error if the property fails
        (left =? right || left < right) |@ sprintf "%f <> %f" left right
    ```
    
    This passed 100k tests no problem.
    
    2:20pm. Without a doubt, this is eq 9. I had not done it right previously.
    
    How did I define it before?
    
    ```
    let R' (o' : Policy) (o : Policy) (tree : GameTree) =
        u (update_at_branch_current o o' tree) tree - u o tree +
        /// According to definition of succ which only considers opponent reach probabilities,
        /// I skip multipling by current player reach probabilities.
        u' o (fun _ branch -> u (update_at_branch_descent o o' branch) branch - u o branch) tree
    ```
    
    With the benefit of hindsight, now I know that I need cycle elimination and the `max 0.0`.
    
    2:30pm. I now get the proof in nearly its entirety. But I am still missing the crucial eq 8 to eq 9 step.
    
    Let me see what Lanctot wrote nearly two weeks ago."

---
+++ CFR In Fsharp/Lemma 5/eq 9.fsx
@@ -0,0 +1,61 @@
+﻿// Intended to test eq 9 from the `Regret Minimization in Games with Incomplete Information` paper with some simplifying assumptions.
+// * Chance nodes are not there.
+// * T is 1.
+// * Policy is not split into actions at current node and everything else.
+// * Only a single player is considered. This is equivalent to the other player having nodes with only a single action.
+
+// All of the above are simplifications of the original definitions and could easily be added. But if the subset does not work, then
+// the full proof won't either.
+
+#load "Core.fsx"
+open Core
+
+let u' (o : Policy) f = function
+    | Terminal reward -> reward
+    | Response (id, branches) -> Array.fold2 (fun s policy branch -> s + f policy branch) 0.0 o.[id] branches
+
+let rec u (o : Policy) tree = u' o (fun policy branch -> policy * u o branch) tree
+
+let update_at_branch_current cur next = function
+    | Terminal _ -> cur
+    | Response (id, _) -> Map.add id (Map.find id next) cur
+
+let action_at i (o : Policy) (id : Infoset) =
+    let a = Array.zeroCreate o.[id].Length
+    a.[i] <- 1.0
+    Map.add id a o
+
+let action_max (o : Policy) (id : Infoset) f =
+    let len = o.[id].Length
+    let rec loop s i =
+        if i < len then
+            let a = Array.zeroCreate len
+            a.[i] <- 1.0
+            loop (max s (f i (Map.add id a o))) (i+1)
+        else s
+    loop -infinity 0
+
+let R (o' : Policy) (o : Policy) (tree : GameTree) = u o' tree - u o tree
+let R' (o' : Policy) (o : Policy) (tree : GameTree) =
+    match tree with
+    | Terminal _ -> 0.0
+    | Response (id, branches) ->
+        action_max o id (fun i o_a ->
+            u o_a tree - u o tree + 
+            // According to definition of succ which only considers opponent reach probabilities, 
+            // I skip multipling by current player reach probabilities.
+            // Since perfect recall assures no cycles in infosets `u o' branch = u (action_at i o' id) branch` holds.
+            u o' branches.[i] - u o branches.[i]
+            )
+
+open FsCheck
+
+let ``R=R'`` ({tree=tree; policies=policies} : TreePolicies) =
+    let o, o' = policies.[0], policies.[1]
+    let left = R o' o tree
+    let right = R' o' o tree
+    // Tests for equality and prints an error if the property fails
+    (left =? right || left <= right) |@ sprintf "%f <> %f" left right
+
+// Fails.
+Check.One({Config.Default with MaxTest=10000}, ``R=R'``)
